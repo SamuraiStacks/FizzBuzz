4e750794e788d30d6a7b31c9cc3000a69553fb73
SamuraiStackszz <38820196+SamuraiStacks@users.noreply.github.com>
2018-11-11 20:45:29 -0400

    files

---
+++ SamBuzz.js
@@ -0,0 +1,12 @@
+let sambuzz;
+for (let i = 0; i < 100; i++) {
+    if (Number.isInteger(i / 3)) {
+        sambuzz = "Sam"
+        if(Number.isInteger(i / 5)) {
+            sambuzz = `${sambuzz}Buzz`
+        }
+        console.log(sambuzz)
+    } else {
+        console.log(i)
+    }
+}
